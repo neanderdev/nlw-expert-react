6cf74d44e152f97e74c3909733c453fa74eba37d
Neander de Souza <neander.faria@hotmail.com>
2024-02-20 09:45:02 -0300

    Aula 02

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'sonner';
 
-import { App } from './app'
+import { App } from './app';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+
+    <Toaster richColors />
   </React.StrictMode>,
 )
